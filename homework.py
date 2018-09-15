0e1201c4316407acde1a095c9e71a4bd12d5ad23
Ivan <marvel-ivan@mail.ru>
2018-09-15 21:51:34 +0300

    HW3

--- homework.py
+++ homework.py
@@ -1,71 +1,30 @@
-# 1
-number = int(input("Your number"))
-
-action = None
-user_action = None
-
-while not action:
-    user_action = input("Bytes(b)? or kByte(kb)")
-    if user_action == 'b':
-        action = 1024
-    elif user_action == 'kb':
-        action = 1.0 / 1024
+def task1():
+    isl = ord(input("Input symbol:"))
+    if ord('a') <= isl <= ord('z') or ord('A') <= isl <= ord('Z'):
+        print("This english letter %s and her code %s" % (chr(isl), isl))
     else:
-        print("Try again")
-
-print(int(number * action))
-
-# 2 task
-number = int(input())
-
-_sum = 0
-_mul = 1
-
-while number > 0:
-    last_sym = number % 10
-    number //= 10
-    _sum += last_sym
-    _mul *= last_sym
-
-# 3 task
-start = input()
-end = input()
-step = input()
-
-for i in range(start, end, step):
-    print(-1.24 * i ** 2 + i)
-
-# 4 task
-
-value = input("Type number")
-
-for i in range(len(value) / 2):
-    if value[i] != value[-(i+1)]:
-        print('Not palindrome')
-        break
-else:
-    print('Palindrome')
-
-# 5 task
-a = [1, 2, -3]
-
-_sum = 0
-_count = 0
-for value in a:
-    if value > 0:
-        _sum += value
-        _count += 1
-print(_sum / (_count or 1))
-
-# new line
-# new line in branch branch_1
-
-# line 1
-# line 2
-# line 3
-
-# line 4
-# line 5
+        print("This other symbol %s and his code %s" % (chr(isl), isl))
+    pass
 
 
+def task2():
+    try:
+        a = int(input("Input numb: "))
+    except ValueError:
+        print("It's not numb, try again")
+    else:
+        fl = 0
+        if a >= 0:
+            fl = 1
+            for i in range(1, a+1):
+                fl *= i
+            print("Your faktorial:", fl)
+    pass
 
+
+def task3():
+    for a in range(1, 11):
+        for b in range(1, 11):
+            print("%5d" % (a * b), end='')
+        print()
+    pass
